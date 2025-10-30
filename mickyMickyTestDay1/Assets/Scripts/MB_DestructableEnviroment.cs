0920eba83c307fb566e20b3d2f4a3413b721c59f
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2025-10-30 16:53:21 -0400

    created and fixed rock prefab

--- mickyMickyTestDay1/Assets/Scripts/MB_DestructableEnviroment.cs
+++ mickyMickyTestDay1/Assets/Scripts/MB_DestructableEnviroment.cs
@@ -1,26 +1,40 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MB_DestructableEnviroment : MonoBehaviour
 {
     public int rockHP;
+    public AudioSource cannonBallFireHit;
+    //public AudioSource audioSource;
+    //public AudioClip cannonBallDestroy;
+
+    private void Start()
+    {
+        //Scrapped audioSource = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
         if (rockHP <= 0)
         {
+            //Scrapped audioSource.PlayOneShot(cannonBallDestroy, 1f);
+            //Does not sound on destroy, cannonBallFireHit.Play();
+            print("rock is destroyed");
             Destroy(this.gameObject);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        print("Player 2 is hit");
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("cannonBall"))
         {
+            print("rock is hit");
+            cannonBallFireHit.Play();
             print(rockHP);
             rockHP -= 3;
 
