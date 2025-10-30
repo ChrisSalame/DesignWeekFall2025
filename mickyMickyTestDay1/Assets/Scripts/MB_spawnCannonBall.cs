e6ac7f6907fa39d02b52196aa561edde59c11dcf
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2025-10-29 20:03:26 -0400

    cannon and boat imported into unity

--- mickyMickyTestDay1/Assets/Scripts/MB_spawnCannonBall.cs
+++ mickyMickyTestDay1/Assets/Scripts/MB_spawnCannonBall.cs
@@ -12,7 +12,7 @@ public class MB_spawnCannonBall : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Alpha5))
         {
             GameObject gO = Instantiate(cannonBalls);
-            gO.transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z +1);
+            gO.transform.position = new Vector3 (transform.position.x , transform.position.y, transform.position.z);
             gO.transform.rotation = transform.rotation;
             gO.SetActive(true);
 
